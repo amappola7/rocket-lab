08a6cf47d79711f76d5b18786cd4ba7ee0eabf0e
Angie-Santos <ajsag12@gmail.com>
2022-11-16 12:36:38 -0500

    Merge

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Capsules from './components/Capsules'
+
+function App() {
+  return (
+    <>
+      <h2>All Capsules:</h2>
+      <Capsules/>
+    </>
+  )
+}
+
+export default App

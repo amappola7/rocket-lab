fb5084de88bd46ec9009525125e03ac85224799f
Angie-Santos <ajsag12@gmail.com>
2022-11-16 18:27:44 -0500

    Capsulas terminadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
-import { Header } from './components/Header.jsx';
-import { StatusCapsules } from './components/StatusCapsules.jsx';
-import { Capsules } from './components/Capsules.jsx';
-import { Footer } from './components/Footer.jsx';
+import { Header } from "./components/Header.jsx";
+import { StatusCapsules } from "./components/StatusCapsules.jsx";
+import Capsules from "./components/Capsules.jsx";
+import { Footer } from "./components/Footer.jsx";
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <h2>All Capsules:</h2>
+      <Capsules />
+    </>
+  );
+}
+
+export default App;
--- src/components/Capsules.jsx
+++ src/components/Capsules.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import './styles/Capsules.scss';
 
-function Capsules() {
+export default function Capsules() {
   return (
       <section className='capsules'>
-        <h5 className='capsules__title'>C#
+        <p className='capsules__title'>C#
           <img src="src/assets/rocketActive.svg" alt="status rocket" />
-        </h5>
+        </p>
         <p className='capsules__description'>Description rocket and history</p>
       </section>
   )
 }
-
-export default Capsules

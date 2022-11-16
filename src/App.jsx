db4dc4c2fa77d5f1b8dff19f87eefcc352e461a3
Amappola <anam.porrasp@gmail.com>
2022-11-16 12:54:05 -0500

    Añadiendo componentes de Footer y StatusCapsules a App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Header } from './components/Header.jsx';
 import { StatusCapsules } from './components/StatusCapsules.jsx';
 import { Capsules } from './components/Capsules.jsx';
 import { Footer } from './components/Footer.jsx';
+
+function App() {
+  <Fragment>
+    <section className="status-capsules">
+      <h2 className="status-capsules__title">Capsules Status</h2>
+      <StatusCapsules className='status-capsules__activate' text='Active' logo='./assets/rocketActive.svg' />
+      <StatusCapsules className='status-capsules__destroyed' text='Destroyed' logo='./assets/rocketDeactivate.svg' />
+      <StatusCapsules className='status-capsules__unknown' text='Unknown' logo='./assets/rocketUnknown.svg' />
+    </section>
+    <Footer/>
+  </Fragment>
+}
+
+export default App;
--- src/components/StatusCapsules.jsx
+++ src/components/StatusCapsules.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function StatusCapsules(props) {
   return (
     <div className="status-capsules">
-      <h2 className="status-capsules__title">{props.text}</h2>
+      <h3 className="status-capsules__title">{props.text}</h3>
       <img className="status-capsules__logo" src={props.logo} alt='logo'/>
     </div>
   )

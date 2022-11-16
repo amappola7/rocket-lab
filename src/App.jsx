6a3c561ac71f17300f73e784efe90ecea59ee436
Amappola <anam.porrasp@gmail.com>
2022-11-16 16:35:56 -0500

    Estilos de los componentes StatusCapsules y Footer terminados

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,25 @@ import { Header } from './components/Header.jsx';
 import { StatusCapsules } from './components/StatusCapsules.jsx';
 import { Capsules } from './components/Capsules.jsx';
 import { Footer } from './components/Footer.jsx';
+import './App.scss'
+import './components/styles/_globals.scss'
 
 function App() {
-  <Fragment>
-    <section className="status-capsules">
-      <h2 className="status-capsules__title">Capsules Status</h2>
-      <StatusCapsules className='status-capsules__activate' text='Active' logo='./assets/rocketActive.svg' />
-      <StatusCapsules className='status-capsules__destroyed' text='Destroyed' logo='./assets/rocketDeactivate.svg' />
-      <StatusCapsules className='status-capsules__unknown' text='Unknown' logo='./assets/rocketUnknown.svg' />
-    </section>
-    <Footer/>
-  </Fragment>
+  return (
+    <Fragment>
+      <section className="status-capsules">
+        <h2 className="subtitle status-capsules__title">Capsules status:</h2>
+        <div className="status-capsules__container">
+          <StatusCapsules className='status-capsules__capsule status-capsules__capsule--activate' text='Active' logo='src/assets/rocketActive.svg' />
+          <StatusCapsules className='status-capsules__capsule status status-capsules__capsule--destroyed' text='Destroyed' logo='src/assets/rocketDeactivate.svg' />
+          <StatusCapsules className='status-capsules__capsule status-capsules__capsule--unknown' text='Unknown' logo='src/assets/rocketUnknown.svg' />
+        </div>
+      </section>
+      <h2 className="subtitle">All Capsules:</h2>
+      <p style={{height: '20px', backgroundColor: 'black', color: 'white',}}>AQUÍ VAN LAS CAPSULAS - BORRAR ESTO</p>
+      <Footer/>
+    </Fragment>
+  )
 }
 
 export default App;
--- src/components/StatusCapsules.jsx
+++ src/components/StatusCapsules.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import './styles/StatusCapsules.scss'
 
 function StatusCapsules(props) {
   return (
-    <div className="status-capsules">
+    <div className={props.className}>
       <h3 className="status-capsules__title">{props.text}</h3>
       <img className="status-capsules__logo" src={props.logo} alt='logo'/>
     </div>
   )
 }
+
 
 export { StatusCapsules };

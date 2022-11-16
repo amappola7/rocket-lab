3bcb3d4206a8041ca87d64205f5ffd573c8a81b2
Angie-Santos <ajsag12@gmail.com>
2022-11-16 18:41:01 -0500

    Ajuste medidas estilos

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Header } from "./components/Header.jsx";
 import { StatusCapsules } from "./components/StatusCapsules.jsx";
 import Capsules from "./components/Capsules.jsx";
 import { Footer } from "./components/Footer.jsx";
-import './App.scss'
-import './components/styles/_globals.scss';
+import "./App.scss";
+import "./components/styles/_globals.scss";
 
 function App() {
   return (
@@ -12,16 +12,32 @@ function App() {
       <section className="status-capsules">
         <h2 className="subtitle status-capsules__title">Capsules status:</h2>
         <div className="status-capsules__container">
-          <StatusCapsules className='status-capsules__capsule status-capsules__capsule--activate' text='Active' logo='src/assets/rocketActive.svg' />
-          <StatusCapsules className='status-capsules__capsule status status-capsules__capsule--destroyed' text='Destroyed' logo='src/assets/rocketDeactivate.svg' />
-          <StatusCapsules className='status-capsules__capsule status-capsules__capsule--unknown' text='Unknown' logo='src/assets/rocketUnknown.svg' />
+          <StatusCapsules
+            className="status-capsules__capsule status-capsules__capsule--activate"
+            text="Active"
+            logo="src/assets/rocketActive.svg"
+          />
+          <StatusCapsules
+            className="status-capsules__capsule status status-capsules__capsule--destroyed"
+            text="Destroyed"
+            logo="src/assets/rocketDeactivate.svg"
+          />
+          <StatusCapsules
+            className="status-capsules__capsule status-capsules__capsule--unknown"
+            text="Unknown"
+            logo="src/assets/rocketUnknown.svg"
+          />
         </div>
       </section>
-      <h2>All Capsules:</h2>
-      <Capsules />
-      <Footer/>
+      <section className="capsule">
+        <h2 className="subtitle capsule__title">All Capsules:</h2>
+        <div className="capsule__container">
+          <Capsules />
+        </div>
+      </section>
+      <Footer />
     </Fragment>
-  )
+  );
 }
 
 export default App;
--- src/components/Capsules.jsx
+++ src/components/Capsules.jsx
@@ -4,9 +4,10 @@ import './styles/Capsules.scss';
 export default function Capsules() {
   return (
       <section className='capsules'>
-        <p className='capsules__title'>C#
+        <h5 className='capsules__title'>
+          C100
           <img src="src/assets/rocketActive.svg" alt="status rocket" />
-        </p>
+        </h5>
         <p className='capsules__description'>Description rocket and history</p>
       </section>
   )
